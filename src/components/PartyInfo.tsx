ead248101cacd8c773237cf652b2c8e12c6899c2
J Chiu <john.chiu@oocl.com>
2025-06-26 17:36:05 +0800

    Add new fields in party addresses

--- src/components/PartyInfo.tsx
+++ src/components/PartyInfo.tsx
@@ -28,55 +28,67 @@ const countries: Country[] = [
   { code: 'IN', name: 'India' },
   { code: 'MY', name: 'Malaysia' },
   { code: 'KR', name: 'South Korea' },
+  { code: 'KH', name: 'Cambodia' },
 ];
 
-// Sample system data (in a real application, this would come from an API)
+// System data with address as 4 lines and new fields
 const systemPartyData = {
   shipper: {
-    name: 'ABC Trading Co., Ltd.',
-    address: ['123 Rama IV Rd.', 'Klongtoey', 'Bangkok', '10110'],
-    country: 'TH',
+    name: 'ABC Logistics Pte Ltd',
+    addressLines: ['10 Changi South Lane', 'Changi', 'Singapore', '486162'],
+    country: 'SG',
+    street: '10 Changi South Lane',
+    city: 'Singapore',
+    state: 'Singapore',
+    postalCode: '486162'
   },
   consignee: {
-    name: 'XYZ Importers Inc.',
-    address: ['456 Market St.', 'Central', 'Hong Kong', ''],
-    country: 'CN',
+    name: 'Phnom Penh Import Export Co., Ltd.',
+    addressLines: ['No. 168, Russian Federation Blvd', 'Tuol Kouk', 'Phnom Penh', '12156'],
+    country: 'KH',
+    street: 'No. 168, Russian Federation Blvd',
+    city: 'Phnom Penh',
+    state: 'Phnom Penh',
+    postalCode: '12156'
   },
   notify: {
-    name: 'Notify Party Example',
-    address: ['789 River Rd.', 'District 1', 'Ho Chi Minh City', '70000'],
-    country: 'VN',
-  },
+    name: 'Bangkok Trading Co., Ltd.',
+    addressLines: ['55 Sukhumvit Rd.', 'Khlong Toei', 'Bangkok', '10110'],
+    country: 'TH',
+    street: '55 Sukhumvit Rd.',
+    city: 'Bangkok',
+    state: 'Bangkok',
+    postalCode: '10110'
+  }
+};
+
+// Editable state with address as 4 lines and new fields, initialized with sample data for demo
+const initialPartyData = {
+  shipper: { ...systemPartyData.shipper },
+  consignee: { ...systemPartyData.consignee },
+  notify: { ...systemPartyData.notify }
 };
 
 const PartyInfo: React.FC = () => {
-  const [partyData, setPartyData] = useState({
-    shipper: {
-      name: 'ABC Trading Co., Ltd.',
-      address: ['123 Rama IV Rd.', 'Klongtoey', 'Bangkok', '10110'],
-      country: 'TH',
-    },
-    consignee: {
-      name: 'XYZ Importers Inc.',
-      address: ['456 Market St.', 'Central', 'Hong Kong', ''],
-      country: 'CN',
-    },
-    notify: {
-      name: 'Notify Party Example',
-      address: ['789 River Rd.', 'District 1', 'Ho Chi Minh City', '70000'],
-      country: 'VN',
-    },
-  });
+  const [partyData, setPartyData] = useState(initialPartyData);
   const [openConfirmDialog, setOpenConfirmDialog] = useState(false);
 
   const handlePartyChange = (party: 'shipper' | 'consignee' | 'notify', field: string, value: string) => {
-    setPartyData(prev => ({
+    setPartyData((prev) => ({
       ...prev,
       [party]: {
         ...prev[party],
-        [field]: field === 'address' ? 
-          prev[party].address.map((addr, i) => i === parseInt(value.split('-')[1]) ? value.split('-')[0] : addr) :
-          value
+        [field]: value
+      }
+    }));
+  };
+
+  const handleAddressLineChange = (party: 'shipper' | 'consignee' | 'notify', lineIdx: number, value: string) => {
+    setPartyData((prev) => ({
+      ...prev,
+      [party]: {
+        ...prev[party],
+        addressLines: prev[party].addressLines.map((line, idx) => idx === lineIdx ? value : line)
       }
     }));
   };
@@ -103,127 +115,135 @@ const PartyInfo: React.FC = () => {
   };
 
   const renderPartySection = (party: 'shipper' | 'consignee' | 'notify', title: string) => (
-    <Grid item xs={12} md={4}>
-      <Paper sx={{ p: 2 }}>
-        <Typography variant="h6" gutterBottom>
-          {title}
-        </Typography>
-        <Box>
-          <Paper 
-            variant="outlined" 
-            sx={{ 
-              p: 1, 
-              mb: 2,
-              bgcolor: 'grey.100',
-              fontSize: '12px',
-              color: 'text.secondary'
-            }}
+    <Box sx={{ mb: 3 }}>
+      <Typography variant="subtitle1" sx={{ mb: 1 }}>{title}</Typography>
+      <Grid container spacing={2} alignItems="center">
+        {/* Name */}
+        <Grid item xs={3}><Typography>Name:</Typography></Grid>
+        <Grid item xs={9}>
+          <TextField
+            fullWidth
+            value={partyData[party].name}
+            onChange={(e) => handlePartyChange(party, 'name', e.target.value)}
+            size="small"
+          />
+        </Grid>
+        {/* Address Lines */}
+        {[0, 1, 2, 3].map((idx) => (
+          <React.Fragment key={idx}>
+            <Grid item xs={3}>
+              {idx === 0 ? <Typography>Address:</Typography> : null}
+            </Grid>
+            <Grid item xs={9}>
+              <TextField
+                fullWidth
+                value={partyData[party].addressLines[idx]}
+                onChange={(e) => handleAddressLineChange(party, idx, e.target.value)}
+                size="small"
+              />
+            </Grid>
+          </React.Fragment>
+        ))}
+        {/* New fields */}
+        <Grid item xs={3}><Typography>Street:</Typography></Grid>
+        <Grid item xs={9}>
+          <TextField
+            fullWidth
+            value={partyData[party].street}
+            onChange={(e) => handlePartyChange(party, 'street', e.target.value)}
+            size="small"
+          />
+        </Grid>
+        <Grid item xs={3}><Typography>City:</Typography></Grid>
+        <Grid item xs={9}>
+          <TextField
+            fullWidth
+            value={partyData[party].city}
+            onChange={(e) => handlePartyChange(party, 'city', e.target.value)}
+            size="small"
+          />
+        </Grid>
+        <Grid item xs={3}><Typography>State:</Typography></Grid>
+        <Grid item xs={9}>
+          <TextField
+            fullWidth
+            value={partyData[party].state}
+            onChange={(e) => handlePartyChange(party, 'state', e.target.value)}
+            size="small"
+          />
+        </Grid>
+        <Grid item xs={3}><Typography>Postal Code:</Typography></Grid>
+        <Grid item xs={9}>
+          <TextField
+            fullWidth
+            value={partyData[party].postalCode}
+            onChange={(e) => handlePartyChange(party, 'postalCode', e.target.value)}
+            size="small"
+          />
+        </Grid>
+        {/* Country moved after Postal Code */}
+        <Grid item xs={3}><Typography>Country:</Typography></Grid>
+        <Grid item xs={9}>
+          <TextField
+            select
+            fullWidth
+            value={partyData[party].country}
+            onChange={(e) => handlePartyChange(party, 'country', e.target.value)}
+            size="small"
           >
-            {systemPartyData[party].name}<br />
-            {systemPartyData[party].address[0]}<br />
-            {systemPartyData[party].address[1]}<br />
-            {systemPartyData[party].address[2]}<br />
-            {systemPartyData[party].address[3]}<br />
-            {countries.find(c => c.code === systemPartyData[party].country)?.name}
-          </Paper>
-          
-          <Grid container spacing={2}>
-            {/* Name */}
-            <Grid item xs={12}>
-              <Grid container spacing={1} alignItems="center">
-                <Grid item xs={3}>
-                  <Typography>Name:</Typography>
-                </Grid>
-                <Grid item xs={9}>
-                  <TextField
-                    required
-                    fullWidth
-                    value={partyData[party].name}
-                    onChange={(e) => handlePartyChange(party, 'name', e.target.value)}
-                    size="small"
-                  />
-                </Grid>
+            {countries.map((country) => (
+              <MenuItem key={country.code} value={country.code}>
+                {country.code} - {country.name}
+              </MenuItem>
+            ))}
+          </TextField>
+        </Grid>
+      </Grid>
+      {/* System Data Display */}
+      <Paper variant="outlined" sx={{ mt: 2, p: 2, bgcolor: 'grey.100', fontSize: '13px', color: 'text.secondary' }}>
+        <strong>System Data</strong><br />
+        <Grid container spacing={2} alignItems="center">
+          {/* Name */}
+          <Grid item xs={3}><Typography>Name:</Typography></Grid>
+          <Grid item xs={9}>{systemPartyData[party].name}</Grid>
+          {/* Address Lines */}
+          {[0, 1, 2, 3].map((idx) => (
+            <React.Fragment key={idx}>
+              <Grid item xs={3}>
+                {idx === 0 ? <Typography>Address:</Typography> : null}
               </Grid>
-            </Grid>
-
-            {/* Address Lines */}
-            <Grid item xs={12}>
-              <Grid container spacing={1}>
-                {[1, 2, 3, 4].map((line) => (
-                  <Grid item xs={12} key={line}>
-                    <Grid container spacing={1} alignItems="center">
-                      {line === 1 ? (
-                        <>
-                          <Grid item xs={3}>
-                            <Typography>Address:</Typography>
-                          </Grid>
-                          <Grid item xs={9}>
-                            <TextField
-                              fullWidth
-                              required={line === 1}
-                              value={partyData[party].address[line - 1] || ''}
-                              onChange={(e) => handlePartyChange(party, 'address', `${e.target.value}-${line - 1}`)}
-                              size="small"
-                            />
-                          </Grid>
-                        </>
-                      ) : (
-                        <Grid item xs={12}>
-                          <Grid container spacing={1}>
-                            <Grid item xs={3} />
-                            <Grid item xs={9}>
-                              <TextField
-                                fullWidth
-                                value={partyData[party].address[line - 1] || ''}
-                                onChange={(e) => handlePartyChange(party, 'address', `${e.target.value}-${line - 1}`)}
-                                size="small"
-                              />
-                            </Grid>
-                          </Grid>
-                        </Grid>
-                      )}
-                    </Grid>
-                  </Grid>
-                ))}
-              </Grid>
-            </Grid>
-
-            {/* Country */}
-            <Grid item xs={12}>
-              <Grid container spacing={1} alignItems="center">
-                <Grid item xs={3}>
-                  <Typography>Country:</Typography>
-                </Grid>
-                <Grid item xs={9}>
-                  <TextField
-                    required
-                    fullWidth
-                    select
-                    value={partyData[party].country}
-                    onChange={(e) => handlePartyChange(party, 'country', e.target.value)}
-                    size="small"
-                  >
-                    {countries.map((country) => (
-                      <MenuItem key={country.code} value={country.code}>
-                        {country.code} - {country.name}
-                      </MenuItem>
-                    ))}
-                  </TextField>
-                </Grid>
-              </Grid>
-            </Grid>
-          </Grid>
-        </Box>
+              <Grid item xs={9}>{systemPartyData[party].addressLines[idx]}</Grid>
+            </React.Fragment>
+          ))}
+          {/* New fields */}
+          <Grid item xs={3}><Typography>Street:</Typography></Grid>
+          <Grid item xs={9}>{systemPartyData[party].street}</Grid>
+          <Grid item xs={3}><Typography>City:</Typography></Grid>
+          <Grid item xs={9}>{systemPartyData[party].city}</Grid>
+          <Grid item xs={3}><Typography>State:</Typography></Grid>
+          <Grid item xs={9}>{systemPartyData[party].state}</Grid>
+          <Grid item xs={3}><Typography>Postal Code:</Typography></Grid>
+          <Grid item xs={9}>{systemPartyData[party].postalCode}</Grid>
+          {/* Country moved after Postal Code */}
+          <Grid item xs={3}><Typography>Country:</Typography></Grid>
+          <Grid item xs={9}>{systemPartyData[party].country}</Grid>
+        </Grid>
       </Paper>
-    </Grid>
+    </Box>
   );
 
   return (
     <Box sx={{ pb: 8 }}>
       <Grid container spacing={2}>
-        {renderPartySection('shipper', 'Shipper')}
-        {renderPartySection('consignee', 'Consignee')}
-        {renderPartySection('notify', 'Notify Party')}
+        <Grid item xs={12} md={4}>
+          {renderPartySection('shipper', 'Shipper')}
+        </Grid>
+        <Grid item xs={12} md={4}>
+          {renderPartySection('consignee', 'Consignee')}
+        </Grid>
+        <Grid item xs={12} md={4}>
+          {renderPartySection('notify', 'Notify Party')}
+        </Grid>
       </Grid>
 
       <DockedButtonPane
